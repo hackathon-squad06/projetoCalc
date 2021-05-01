5be713ac85f4d22cd1a3803fa85b0e4787ae69f7
Gabriel Dutra <gfhdutra@gmail.com>
2021-05-01 15:31:31 -0300

    ajustado modo escuro nas duas páginas

--- home/darkModeHome.js
+++ home/darkModeHome.js
@@ -8,10 +8,28 @@ function switchMode() {
   if (html.classList.contains('dark-mode')) {
     logoSquadHeader.src = "/images/squad06-logo-white.svg";
     switchDarkMode.src = "/images/light-mode-white.svg";
+    const darkModeHome = true
+    sessionStorage.setItem('darkModeHome', darkModeHome)
   } else {
     logoSquadHeader.src = "/images/squad06-logo.svg";
     switchDarkMode.src = "/images/dark-mode-black.svg";
+    const darkModeHome = false
+    sessionStorage.setItem('darkModeHome', darkModeHome)
   }
 }
 
-// switchMode()
+function getDarkMode() {
+  let darkModeResult = sessionStorage.getItem('darkModeResult')
+  if (darkModeResult) {
+    if (html.classList.contains('dark-mode')) {
+      return
+    }
+    else {
+      switchMode()
+      sessionStorage.removeItem('darkModeHome')
+      sessionStorage.removeItem('darkModeResult')
+    }
+  }
+}
+
+getDarkMode()
