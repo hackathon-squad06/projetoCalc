e82f2d74cbe05618efa642971860a30576cd5c3f
Bruno Kamui <brunorbrasil@gmail.com>
2021-05-02 16:03:32 -0300

    Adicionado JS para o input e outras correcoes de layout

---
+++ home/inputCurrency.js
@@ -0,0 +1,29 @@
+function formatarReais() {
+    var digitou = document.querySelector("#inputValorImovel");
+    var valor = digitou.value;
+
+    valor = valor + '';
+    valor = parseInt(valor.replace(/[\D]+/g, ''));
+    valor = valor + '';
+    valor = valor.replace(/(\d{2})$/, ",$1");
+
+    if (valor.length > 6) {
+        valor = valor.replace(/(\d{3}),(\d{2}$)/, ".$1,$2");
+    }   if (valor.length > 10) {
+        valor = valor.replace(/(\d{3}).(\d{3}),(\d{2})/, ".$1.$2,$3");
+    }
+
+    digitou.value = "R$" + valor;
+    if (valor == 'NaN') {
+        digitou.value = '';
+    }
+    
+    //Transforma a string em número
+    digitou.onblur = function () {
+      console.log(valor)
+      valor = valor.replace(/[.]/g, "")
+      valor = valor.replace(/[,]/g, ".")
+      valor = Number(valor)
+      console.log(valor)
+    }
+}
