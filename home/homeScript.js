d5acc51a5dee909a988d190400375ac02b0c83e9
Leonardo Costa <leonardocg.lcg@gmail.com>
2021-05-03 10:39:34 -0300

    math ceil das variaveis e armazenamento delas em SessionStorage

--- home/homeScript.js
+++ home/homeScript.js
@@ -1,18 +1,12 @@
 function calcular() {
-  const valorImovel = parseFloat(
-    document.getElementById("inputValorImovel").value
-  );
+  const valorImovel = parseFloat(document.getElementById("inputValorImovel").value);
   const aluguel = parseFloat(document.getElementById("inputAluguel").value);
-  const valorizacao = parseFloat(
-    document.getElementById("inputValorizacao").value
-  );
+  const valorizacao = parseFloat(document.getElementById("inputValorizacao").value);
   const entrada = parseFloat(document.getElementById("inputEntrada").value);
   const custosAd = parseFloat(document.getElementById("inputCustosAd").value);
   const prazoFin = parseFloat(document.getElementById("inputPrazoFin").value);
   const taxaFin = parseFloat(document.getElementById("inputTaxaFin").value);
-  const rentabilidade = parseFloat(
-    document.getElementById("inputRentabilidade").value
-  );
+  const rentabilidade = parseFloat(document.getElementById("inputRentabilidade").value);
 
   const c = entrada;
   const i = ((1 + rentabilidade / 100) ** (1 / 12) - 1) * 100;
@@ -23,33 +17,29 @@ function calcular() {
   const valorizacaoImovelF = valorizacaoImovel.toFixed(2);
   const n = prazoFin * 12;
   const valorFinanciado = valorImovel - c;
-  const parcela =
-    valorFinanciado *
-    (((1 + iFin / 100) ** n * iFin) / 100 / ((1 + iFin / 100) ** n - 1));
+  const parcela = valorFinanciado * (((1 + iFin / 100) ** n * iFin) / 100 / ((1 + iFin / 100) ** n - 1));
   const parcelaF = parcela.toFixed(2);
-  const totalPago = parcelaF * n;
+  const totalPago = Math.ceil(parcelaF * n);
   const valorImovelFuturo = valorImovel * (1 + valorizacaoImovelF / 100) ** n;
   const valorImovelFuturoF = valorImovelFuturo.toFixed(2);
   const valorInvestidoMensal = parcelaF - aluguel;
   const valorInvestidoMensalF = valorInvestidoMensal.toFixed(0);
   const depositoInicial = entrada + custosAd;
   if (totalPago > valorFinanciado) {
-    var totalJurosPago = totalPago - valorFinanciado;
+    var totalJurosPago = Math.ceil(totalPago - valorFinanciado);
   } else {
-    var totalJurosPago = valorFinanciado - totalPago;
+    var totalJurosPago = Math.ceil(valorFinanciado - totalPago);
   }
-  const m =
-    c * (1 + iF / 100) ** n +
-    valorInvestidoMensalF * (((1 + iF / 100) ** n - 1) / (iF / 100));
-  const mF = m.toFixed(2);
+  const m = c * (1 + iF / 100) ** n + valorInvestidoMensalF * (((1 + iF / 100) ** n - 1) / (iF / 100));
+  const mF = m.toFixed(0);
   function nper (rate, pmt, pv, fv) {
     const z = pmt * (1 + (rate/100) * 1) / (rate/100)
     return Math.log10((-fv + z) / (pv + z)) / Math.log10(1 + (rate/100))
   }
   const prazoRentabilidade = Math.ceil(nper(iF, -valorInvestidoMensalF, -depositoInicial, valorImovelFuturoF))
   const prazoRentabilidadeAnos = Math.ceil(prazoRentabilidade/12)
-  const valorPagoFinanciamento = c + totalPago
-  const sobraInvestimento = mF - valorImovelFuturoF
+  const valorPagoFinanciamento = Math.ceil(c + totalPago)
+  const sobraInvestimento = Math.ceil(mF - valorImovelFuturoF)
   console.log(
     c,
     iF,
@@ -68,12 +58,22 @@ function calcular() {
     sobraInvestimento
   );
 
-//   sessionStorage.setItem('valorImovel', valorImovel)
-//   sessionStorage.setItem('valorImovelFuturo', valorImovelFuturoF)
-//   sessionStorage.setItem('valorTotalPago', totalPago)
-//   sessionStorage.setItem('valorJurosPago', totalJurosPago)
-//   sessionStorage.setItem('valorInvestimentoTotal', mF)
-//   window.location.href = "/resultap/resultap.html"
+  sessionStorage.setItem('valorImovel', valorImovel)
+  sessionStorage.setItem('valorImovelFuturo', valorImovelFuturoF)
+  sessionStorage.setItem('valorTotalPago', totalPago)
+  sessionStorage.setItem('valorJurosPago', totalJurosPago)
+  sessionStorage.setItem('valorInvestimentoTotal', mF)
+  sessionStorage.setItem('entrada', c)
+  sessionStorage.setItem('taxaRendimento', iF)
+  sessionStorage.setItem('taxaFinanciamento', iFinF)
+  sessionStorage.setItem('prazoFinanciamento', n)
+  sessionStorage.setItem('parcelaFinanciamento', parcelaF)
+  sessionStorage.setItem('valorInvestimentoMensalmente', valorInvestidoMensalF)
+  sessionStorage.setItem('valorDepositoInicial', depositoInicial)
+  sessionStorage.setItem('prazoRentabilidadeAnos', prazoRentabilidadeAnos)
+  sessionStorage.setItem('valorTotalPagoFinancimento', valorPagoFinanciamento)
+  sessionStorage.setItem('sobraInvestimento', sobraInvestimento)
+  window.location.href = "/resultap/resultap.html"
 }
 
 document.getElementById("inputEntrada").addEventListener("change", function () {
@@ -91,9 +91,7 @@ document.getElementById("inputEntrada").addEventListener("change", function () {
   }
 });
 
-document
-  .getElementById("inputCustosAd")
-  .addEventListener("change", function () {
+document.getElementById("inputCustosAd").addEventListener("change", function () {
     let depositoInicial = document.getElementsByName("inputDeposito")[0];
     let valorEntrada = parseFloat(
       document.getElementById("inputEntrada").value
@@ -108,11 +106,9 @@ document
     } else {
       depositoInicial.placeholder = deposito;
     }
-  });
+});
 
-document
-  .getElementById("inputValorImovel")
-  .addEventListener("change", function () {
+document.getElementById("inputValorImovel").addEventListener("change", function () {
     let depositoMensal = document.getElementsByName("inputValorMensal")[0];
     let valorImovel = parseFloat(
       document.getElementById("inputValorImovel").value
@@ -141,7 +137,7 @@ document
     } else {
       depositoMensal.placeholder = investimentoMensal2;
     }
-  });
+});
 
 document.getElementById("inputEntrada").addEventListener("change", function () {
   let depositoMensal = document.getElementsByName("inputValorMensal")[0];
@@ -197,9 +193,7 @@ document.getElementById("inputAluguel").addEventListener("change", function () {
   }
 });
 
-document
-  .getElementById("inputPrazoFin")
-  .addEventListener("change", function () {
+document.getElementById("inputPrazoFin").addEventListener("change", function () {
     let depositoMensal = document.getElementsByName("inputValorMensal")[0];
     let valorImovel = parseFloat(
       document.getElementById("inputValorImovel").value
@@ -228,7 +222,7 @@ document
     } else {
       depositoMensal.placeholder = investimentoMensal2;
     }
-  });
+});
 
 document.getElementById("inputTaxaFin").addEventListener("change", function () {
   let depositoMensal = document.getElementsByName("inputValorMensal")[0];
