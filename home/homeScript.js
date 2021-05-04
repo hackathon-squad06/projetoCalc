1657e07ac38d13d181d6a71daa5d499bd04a5019
Gabriel Dutra <gfhdutra@gmail.com>
2021-05-04 15:52:19 -0300

    resolvidos bugs envolvendo os campos em porcentagem e em anos

--- home/homeScript.js
+++ home/homeScript.js
@@ -1,9 +1,9 @@
 
 function calcular() {
-  const valorizacao = parseFloat(document.getElementById('inputValorizacao').value);
-  const prazoFin = parseFloat(document.getElementById('inputPrazoFin').value);
-  const taxaFin = parseFloat(document.getElementById('inputTaxaFin').value);
-  const rentabilidade = parseFloat(document.getElementById('inputRentabilidade').value);
+  const valorizacao = realParseFloat(document.getElementById('inputValorizacao').value);
+  const prazoFin = realParseFloat(document.getElementById('inputPrazoFin').value);
+  const taxaFin = realParseFloat(document.getElementById('inputTaxaFin').value);
+  const rentabilidade = realParseFloat(document.getElementById('inputRentabilidade').value);
 
   const c = valorDaEntrada
   const i = (((1 + (rentabilidade / 100)) ** (1 / 12)) - 1) * 100
@@ -37,10 +37,10 @@ function calcular() {
   const prazoRentabilidadeAnos = Math.ceil(prazoRentabilidade / 12)
   const valorPagoFinanciamento = Math.ceil(c + totalPago)
   const sobraInvestimento = Math.ceil(mF - valorImovelFuturoF)
-  if ( mF >= valorImovelFuturoF ) {
-	var melhorOpcao = 'Alugar'
+  if (mF >= valorImovelFuturoF) {
+    var melhorOpcao = 'Alugar'
   } else {
-	var melhorOpcao = 'Financiar'
+    var melhorOpcao = 'Financiar'
   }
   console.log(
     c,
@@ -183,8 +183,8 @@ function calcDeposito(valorDaEntrada, custosAdicionais) {
 
 function valorInvestimentoMensal() {
   let investimentoMensal = document.getElementsByName('inputValorMensal')[0];
-  let taxaFin = parseFloat(document.getElementById('inputTaxaFin').value);
-  let prazoFin = parseFloat(document.getElementById('inputPrazoFin').value);
+  let taxaFin = realParseFloat(document.getElementById('inputTaxaFin').value);
+  let prazoFin = realParseFloat(document.getElementById('inputPrazoFin').value);
 
   let valorFinanciado = valorDeCompra - valorDaEntrada
   let prazoFinMes = prazoFin * 12
@@ -198,5 +198,106 @@ function valorInvestimentoMensal() {
     investimentoMensal.placeholder = 'Adicione os dados para o cálculo'
   } else {
     investimentoMensal.placeholder = 'R$ ' + investimentoMensalFinal + ',00';
+  }
+}
+
+
+// function formatarAnos(element) {
+//   var id = element.id
+//   var campo = document.getElementById(id)
+//   var valor = campo.value
+//   var n = valor.toLocaleString();
+
+//   if (valor == '' || valor < 0 || isNaN(valor)) {
+//     console.log('é NaN')
+//     campo.type = 'text';
+//     campo.value = null;
+//   } else {
+//     campo.type = 'text';
+//     valor = n + ' anos';
+//     campo.value = valor;
+//   }
+//   campo.onclick = function () {
+//     campo.type = 'text';
+//     valor = n + ' em anos';
+//     campo.value = valor;
+//     campo.type = 'number';
+//   }
+// }
+
+function formatarAnos(element) {
+  var id = element.id
+  var campo = document.getElementById(id)
+  var valor = campo.value
+  var containWord = /[a-z]/i.test(valor)
+  var containSimbol = /[`~!@#$%^&*()_|+\-=?;:'"<>\{\}\[\]\\\/]/gi.test(valor)
+
+  if (containWord || containSimbol) {
+    console.log('contem caracteres')
+    valorA = valor.replace(/[`~!@#$%^&*()_|+\-=?;:'"<>\{\}\[\]\\\/]/gi, '')
+    valor = valorA.replace(/[\D]+/g, '')
+    campo.value = valor;
+  }
+  if (valor == '' || valor < 0) {
+    console.log('é NaN')
+    valor = ''
+    campo.value = null;
+  } else if (campo.value.includes('anos')) {
+    valor = valor;
+    campo.value = valor;
+  } else {
+    valor = valor + ' anos';
+    campo.value = valor;
+  }
+  campo.onclick = function () {
+    valor = valor.replace(' anos', '');
+    campo.value = valor;
+  }
+}
+
+function formatarPorcentagem(element) {
+  var id = element.id
+  var campo = document.getElementById(id)
+  var valor = campo.value
+  var containWord = /[a-z]/i.test(valor)
+  var containSimbol = /[`~!@#$%^&*()_|+\-=?;:'"<>\{\}\[\]\\\/]/gi.test(valor)
+
+  if (containWord || containSimbol) {
+    console.log('contem caracteres')
+    valorA = valor.replace(/[`~!@#$%^&*()_|+\-=?;:'"<>\{\}\[\]\\\/]/gi, '')
+    valor = valorA.replace(/[\D]+/g, '')
+    campo.value = valor;
+  }
+  if (valor == '' || valor < 0) {
+    console.log('é NaN')
+    valor = ''
+    campo.value = null;
+  } else if (campo.value.includes('%')) {
+    valor = valor;
+    campo.value = valor;
+  } else {
+    valor = valor + ' %';
+    campo.value = valor;
+  }
+  campo.onclick = function () {
+    valor = valor.replace(' %', '');
+    campo.value = valor;
+  }
+}
+
+function realParseFloat(s) {
+  s = s.replace(/[^\d,.-]/g, '');
+  if (navigator.language.substring(0, 2) !== "de" && /^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?$/.test(s)) {
+    s = s.replace(/,/g, '');
+    return parseFloat(s);
+  }
+  else if (/^-?(?:\d+|\d{1,3}(?:\.\d{3})+)(?:,\d+)?$/.test(s)) {
+    s = s.replace(/\./g, '');
+    s = s.replace(/,/g, '.');
+    return parseFloat(s);
+  }
+  else {
+    s = s.replace(/,/g, '');
+    return parseFloat(s);
   }
 }
