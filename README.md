ae1081f5251953535859517e2d4e5a0df595513d
Gabriel Dutra <gfhdutra@gmail.com>
2021-05-05 20:56:10 -0300

    ajuste nas cores e design geral do site

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # projetoCalc
 Projeto Hackathon - Squad 06
 
-feature-paginaInicial branch
+develop branch
 
-Feature focada em criar a calculadora de comparação entre uso de carro e uso de app de corridas.
--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -5,22 +5,20 @@ const logoSquadHeader = document.getElementById('logoSquadHeader');
 const currentTheme = localStorage.getItem("theme");
 
 if (currentTheme == "dark") {
-  html.classList.add('dark-mode');
-  logoSquadHeader.src = "/images/s6-logo-white.png";
-  switchDarkMode.src = "/images/light-mode-white.svg";
+    html.classList.add('dark-mode');
+    switchDarkMode.src = "/images/light-mode-white.svg";
 }
 
 switchDarkMode.addEventListener("click", function () {
-  const html = document.querySelector('html');
-  html.classList.toggle('dark-mode');
+    const html = document.querySelector('html');
+    html.classList.toggle('dark-mode');
 
-  let theme = "light";
-  logoSquadHeader.src = "/images/s6-logo-black.png";
-  switchDarkMode.src = "/images/dark-mode-black.svg";
-  if (html.classList.contains('dark-mode')) {
-    theme = "dark";
-    logoSquadHeader.src = "/images/s6-logo-white.png";
-    switchDarkMode.src = "/images/light-mode-white.svg";
-  }
-  localStorage.setItem("theme", theme);
+    let theme = "light";
+    switchDarkMode.src = "/images/dark-mode-white.svg";
+    if (html.classList.contains('dark-mode')) {
+        theme = "dark";
+
+        switchDarkMode.src = "/images/light-mode-white.svg";
+    }
+    localStorage.setItem("theme", theme);
 });
