17d4bfc5f81dc1b8cd139159dacd2f7827fa5d20
Gabriel Dutra <gfhdutra@gmail.com>
2021-05-06 13:25:53 -0300

    adicionado copyright no footer

--- scripts/menuMobile.js
+++ scripts/menuMobile.js
@@ -8,3 +8,22 @@ onload = function () {
         }
     });
 };
+
+var modal = document.getElementsByClassName("menuMobile");
+window.addEventListener('click', function (e) {
+    if (outsideClick(e, modal)) {
+        document.querySelector(".menu").style.display = "none";
+    }
+});
+
+function outsideClick(event, notelem) {
+    // check outside click for multiple elements
+    var clickedOut = true, i, len = notelem.length;
+    for (i = 0; i < len; i++) {
+        if (event.target == notelem[i] || notelem[i].contains(event.target)) {
+            clickedOut = false;
+        }
+    }
+    if (clickedOut) return true;
+    else return false;
+}
