46427b4e9f57410d17b41023ed5e6bbe677491a1
Bruno Kamui <brunorbrasil@gmail.com>
2021-05-07 14:37:16 -0300

    Adicionado botao para subir a tela

---
+++ scripts/scrollUp.js
@@ -0,0 +1,19 @@
+botao = document.querySelector("#btnScroll")
+botao.addEventListener("click", scrollUp)
+
+function scrollUp () {
+    window.scrollTo ({
+        top:0,
+        behavior: "smooth"
+    });
+}
+
+function btnScroll () {
+    if (window.scrollY === 0) {
+        document.querySelector("#btnScroll").style.display = "none";
+    } else {
+        document.querySelector("#btnScroll").style.display = "block";
+    }
+}
+
+window.addEventListener("scroll", btnScroll)
